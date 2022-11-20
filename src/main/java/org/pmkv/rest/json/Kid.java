7c89925b1f414865da23c8fbcb28e5240e3a1869
Petter Kvalvaag <pmkv@equinor.com>
2022-11-20 23:21:26 +0100

    Added transaction DB object

--- src/main/java/org/pmkv/rest/json/Kid.java
+++ src/main/java/org/pmkv/rest/json/Kid.java
@@ -1,11 +1,15 @@
 package org.pmkv.rest.json;
-
+import java.util.Set;
+import java.util.HashSet;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
 import javax.persistence.QueryHint;
 import javax.persistence.SequenceGenerator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @NamedQuery(
@@ -17,6 +21,14 @@ public class Kid {
     private Long id;
     public String name;
     public double initial_balance;
+
+    @JsonIgnore
+    @OneToMany(mappedBy="kid", cascade = CascadeType.ALL)
+    Set<Transaction> transactions = new HashSet<Transaction>();
+
+    // public Set<Transaction> getTransactions() {
+    //     return transactions;
+    // }
 
     @Id
     @SequenceGenerator(name = "kidSeq", sequenceName = "kid_id_seq", allocationSize = 1, initialValue = 1)
---
+++ src/main/java/org/pmkv/rest/json/TransactionDTO.java
@@ -0,0 +1,8 @@
+package org.pmkv.rest.json;
+
+public class TransactionDTO {
+    public Long id;
+    public double change;
+    public Long kid_id;
+    public String description;
+}
