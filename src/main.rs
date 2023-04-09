c18a5fc91818e343306ab1a636dff6d28807bd5c
Petter Kvalvaag <petter.kvalvaag@proton.me>
2023-04-09 21:19:09 +0200

    Add CORS support

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,11 @@ mod schema;
 #[macro_use] extern crate rocket;
 
 use models::{Kid, NewTransaction, Transaction};
+use rocket::fairing::{Fairing, Info, Kind};
 use rocket::data::{Data, FromData, self};
-use rocket::http::Status;
+use rocket::http::{Status, Header};
 use rocket::request::{Outcome, Request, FromRequest};
+use rocket::{Response};
 use rocket::serde::json::{serde_json, Json};
 use rocket::tokio::io::AsyncReadExt;
 use crate::crud::process_user;
@@ -125,14 +127,45 @@ async fn get_transactions(user: User, kid_id: i32) -> Json<Vec<Transaction>> {
     Json(crud::get_transactions(&user, &kid_id).await)
 }
 
+/// Catches all OPTION requests in order to get the CORS related Fairing triggered.
+#[options("/<_..>")]
+fn all_options() {
+    /* Intentionally left empty */
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![
+    rocket::build()
+        .attach(Cors)
+        .mount("/", routes![
         index,
+        all_options,
         user,
         get_kids,
         create_kid,
         create_transaction,
         get_transactions,
     ])
+}
+
+pub struct Cors;
+
+#[rocket::async_trait]
+impl Fairing for Cors {
+    fn info(&self) -> Info {
+        Info {
+            name: "Cross-Origin-Resource-Sharing Fairing",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, PATCH, PUT, DELETE, HEAD, OPTIONS, GET",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
 }
